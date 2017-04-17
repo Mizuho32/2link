e086cee293856c219349722f0f2036fb25b1e85c
Mizuho <dummy32@hotmail.co.jp>
2017-04-17 12:01:24 +0900

    constraint force update. test ok (?)

--- Fc_main.m
+++ Fc_main.m
@@ -1,5 +1,5 @@
 function [f] = Fc_DAC(t, x)
-global l1 l2 m1 m2 g
+global l1 l2 m1 m2 I1 I2 g
 
 x = x';
 
@@ -14,8 +14,8 @@ dt2 = x(4, :);
 tau1 = zeros(size(t))';
 tau2 = tau1;
 
-f1 = (lg1.*m1+l1.*m2).*cos(t1).*(dt1).^2+((-l1.*(lg1.*m1+l1.*m2).*cos(t1-t2).*sin(t1)+(lg1.^2.*m1+l1.^2.*m2).*sin(t2)).*(tau2-g.*lg2.*m2.*cos(t2)+l1.*lg2.*m2.*sin(t1-t2).*(dt1).^2))/(lg2.*(lg1.^2.*m1+l1.^2.*m2-l1.^2.*m2.*cos(t1-t2).^2))+lg2.*m2.*cos(t2).*(dt2).^2-(((2.*lg1.*m1+l1.*m2).*sin(t1)+l1.*m2.*sin(t1-2.*t2)).*(-tau1+g.*(lg1.*m1+l1.*m2).*cos(t1)+l1.*lg2.*m2.*sin(t1-t2).*(dt2).^2))/(2.*lg1.^2.*m1+l1.^2.*m2-l1.^2.*m2.*cos(2.*(t1-t2)));
-f2 = -g.*(m1+m2)+(lg1.*m1+l1.*m2).*sin(t1).*(dt1).^2+((l1.*(lg1.*m1+l1.*m2).*cos(t1).*cos(t1-t2)-(lg1.^2.*m1+l1.^2.*m2).*cos(t2)).*(tau2-g.*lg2.*m2.*cos(t2)+l1.*lg2.*m2.*sin(t1-t2).*(dt1).^2))/(lg2.*(lg1.^2.*m1+l1.^2.*m2-l1.^2.*m2.*cos(t1-t2).^2))+lg2.*m2.*sin(t2).*(dt2).^2+(((lg1.*m1+l1.*m2).*cos(t1)-l1.*m2.*cos(t1-t2).*cos(t2)).*(-tau1+g.*(lg1.*m1+l1.*m2).*cos(t1)+l1.*lg2.*m2.*sin(t1-t2).*(dt2).^2))/(lg1.^2.*m1+l1.^2.*m2-l1.^2.*m2.*cos(t1-t2).^2);
+f1 = ((lg1.*m1+l1.*m2).*cos(t1)+lg2.*m2.*cos(t1+t2)).*(dt1).^2+(((lg1.*m1+l1.*m2).*(I2+lg2.^2.*m2+l1.*lg2.*m2.*cos(t2)).*sin(t1)-lg2.*m2.*(I1+lg1.^2.*m1+l1.^2.*m2+l1.*lg2.*m2.*cos(t2)).*sin(t1+t2)).*(-tau2+g.*lg2.*m2.*cos(t1+t2)+l1.*lg2.*m2.*sin(t2).*(dt1).^2))/((I1+lg1.^2.*m1+l1.^2.*m2).*(I2+lg2.^2.*m2)-l1.^2.*lg2.^2.*m2.^2.*cos(t2).^2)+2.*lg2.*m2.*cos(t1+t2).*(dt1).*(dt2)+lg2.*m2.*cos(t1+t2).*(dt2).^2-(((lg1.*m1+l1.*m2).*(I2+lg2.^2.*m2).*sin(t1)-l1.*lg2.^2.*m2.^2.*cos(t2).*sin(t1+t2)).*(-tau1+g.*lg1.*m1.*cos(t1)+g.*l1.*m2.*cos(t1)+g.*lg2.*m2.*cos(t1+t2)-2.*l1.*lg2.*m2.*sin(t2).*(dt1).*(dt2)-l1.*lg2.*m2.*sin(t2).*(dt2).^2))/((I1+lg1.^2.*m1+l1.^2.*m2).*(I2+lg2.^2.*m2)-l1.^2.*lg2.^2.*m2.^2.*cos(t2).^2);
+f2 = 	-g.*(m1+m2)+((lg1.*m1+l1.*m2).*sin(t1)+lg2.*m2.*sin(t1+t2)).*(dt1).^2-(((lg1.*m1+l1.*m2).*cos(t1).*(I2+lg2.^2.*m2+l1.*lg2.*m2.*cos(t2))-lg2.*m2.*(I1+lg1.^2.*m1+l1.^2.*m2+l1.*lg2.*m2.*cos(t2)).*cos(t1+t2)).*(-tau2+g.*lg2.*m2.*cos(t1+t2)+l1.*lg2.*m2.*sin(t2).*(dt1).^2))/((I1+lg1.^2.*m1+l1.^2.*m2).*(I2+lg2.^2.*m2)-l1.^2.*lg2.^2.*m2.^2.*cos(t2).^2)+2.*lg2.*m2.*sin(t1+t2).*(dt1).*(dt2)+lg2.*m2.*sin(t1+t2).*(dt2).^2+(((lg1.*m1+l1.*m2).*(I2+lg2.^2.*m2).*cos(t1)-l1.*lg2.^2.*m2.^2.*cos(t2).*cos(t1+t2)).*(-tau1+g.*lg1.*m1.*cos(t1)+g.*l1.*m2.*cos(t1)+g.*lg2.*m2.*cos(t1+t2)-2.*l1.*lg2.*m2.*sin(t2).*(dt1).*(dt2)-l1.*lg2.*m2.*sin(t2).*(dt2).^2))/((I1+lg1.^2.*m1+l1.^2.*m2).*(I2+lg2.^2.*m2)-l1.^2.*lg2.^2.*m2.^2.*cos(t2).^2);
 
 f = [f1; f2];
 end
--- forreport.m
+++ forreport.m
@@ -17,9 +17,15 @@ title('y成分');
 figure(8)
 clf
 
+max_Fc = max(diag(sqrt(Fc'*Fc)));
+
 hold on;
 plot(c1(1,:), c1(2,:));
 plot(c2(1,:), c2(2,:));
+for i = 1:length(Fc)
+  tmp = Fc(:, i)/max_Fc;
+  %quiver(0, 0, tmp(1), tmp(2));
+end
 
 xlabel('x');
 ylabel('y');
@@ -31,3 +37,17 @@ clf
 
 xd = Xd(t');
 plot(xd(1,:), xd(2,:));
+
+figure(10)
+
+subplot(2,1,1);
+plot(t, Fc(1,:));
+xlabel('time [s]');
+ylabel('Fcx [N]');
+title('x成分');
+
+subplot(2,1,2)
+plot(t, Fc(2,:));
+xlabel('time [s]');
+ylabel('Fcy [N]');
+title('y成分');
